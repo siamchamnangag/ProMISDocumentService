9638bf6913aae86ef308c878bb49dcb374d6b74f
Jon Randy <jonrandy@gmail.com>
2017-07-13 13:57:24 +0700

    add unit test to controller

---
+++ src/test/java/com/scg/document/controller/DocumentControllerTest.java
@@ -0,0 +1,19 @@
+package com.scg.document.controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by marcus on 7/13/2017 AD.
+ */
+public class DocumentControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCreateDocumentFromTemplate() throws Exception {
+
+    }
+}
