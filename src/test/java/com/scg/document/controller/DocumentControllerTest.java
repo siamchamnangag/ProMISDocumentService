c8f2f53476870df06841c1a4d38b01b4847fbb82
Jon Randy <jonrandy@gmail.com>
2017-07-14 11:21:31 +0700

    add controller test for document post

--- src/test/java/com/scg/document/controller/DocumentControllerTest.java
+++ src/test/java/com/scg/document/controller/DocumentControllerTest.java
@@ -1,19 +1,57 @@
 package com.scg.document.controller;
 
+import com.scg.document.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
- * Created by marcus on 7/13/2017 AD.
+ * Created by tanatloke on 7/13/2017 AD.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class})
+@WebAppConfiguration
 public class DocumentControllerTest {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
     }
 
     @org.junit.Test
-    public void testCreateDocumentFromTemplate() throws Exception {
+    public void testCreateDocumentFromTemplateShouldSuccess() throws Exception {
+        //
+        MvcResult result = mockMvc.perform(post("/documents/1")).andExpect(status().isOk()).andReturn();
+        String content = result.getResponse().getContentAsString();
 
+        assertTrue(content.startsWith("https://github.com/siamchamnangag/ProMISFileService/raw/master/src/main/resources/PMOC_"));
+        assertTrue(content.endsWith(".xlsx"));
     }
+
 }
